f4e19419903f2efc23887dce4316e9c6fc04c93e
Malikhai <101726@glr.nl>
2025-10-16 11:32:10 +0000

    database conncectie script en een paar tabellen opgezet

---
+++ backend/php/conn.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "127.0.0.1";
+$username = "stemwijs";
+$password = "stemwijzer1";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=stemwijs", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "good database";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
